ecca2f6f671e77ed39ab27cd275392a935f70799
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-28 22:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 
 
-document.body.innerHTML = `
-  <h1 id="verification_heading">Verify Your Account</h1>
-  <p id="verification_subtext">Please enter the 6-digit code sent to your device.</p>
-  <div class="code-container">
-    <input type="text" class="code" maxlength="1" id="code-1" >
-    <input type="text" class="code" maxlength="1" id="code-2" >
-    <input type="text" class="code" maxlength="1" id="code-3" >
-    <input type="text" class="code" maxlength="1" id="code-4" >
-    <input type="text" class="code" maxlength="1" id="code-5" >
-    <input type="text" class="code" maxlength="1" id="code-6" >
-  </div><br><br>
-  <p id="submit">This is design only.we didn't actually send you an email
-  as we don't have your email,right</p>
-`;
 
 const codeInputs = document.querySelectorAll('.code');
 
