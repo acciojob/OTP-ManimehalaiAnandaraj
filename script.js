664fc7a120d07cc4244b434142d49ff13be9e174
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-28 22:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-const firstInput = document.getElementById('code-1');
-firstInput.addEventListener('input', () => 
-	{
+
 document.body.innerHTML = `
   <h1 id="verification_heading">Verify Your Account</h1>
   <p id="verification_subtext">Please enter the 6-digit code sent to your device.</p>
@@ -35,4 +33,4 @@ codeInputs.forEach((input, index) => {
     }
   });
 });
-	});
+	
