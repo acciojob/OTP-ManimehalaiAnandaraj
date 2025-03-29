1215ebb107f33a35f9f066b74c8522cce53d3a81
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-29 10:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const codeInputs = document.querySelectorAll('.code');
 
 codeInputs.forEach((input, index) => {
 
-  input.addEventListener('input', () => {
-    if (input.value) {
+  input.addEventListener('keydown', (event) => {
+    if !isNaN(event.key)) {
       if (index < codeInputs.length - 1) {
         codeInputs[index + 1].focus();
       }
     }
   });
-  input.addEventListener('keydown', (event) => {
+  input.addEventListener('keyup', (event) => {
 	 
     if (event.key === 'Backspace') {
       if (!input.value && index > 0) {
