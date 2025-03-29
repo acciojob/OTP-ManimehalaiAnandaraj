cdc73a002ea9b98bf6cc0a683e7a3857ccb455c5
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-29 11:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInputs = document.querySelectorAll('.code');
 codeInputs.forEach((input, index) => {
   input.addEventListener('keydown', (event) => {
     if (/\d/.test(event.key) && index < codeInputs.length - 1) {
-      setTimeout(() => codeInputs[index + 1].focus(), 0);
+      setTimeout(() => codeInputs[index + 1].focus(), 10);
     }
   });
   input.addEventListener('keyup', (event) => {
